e2a58cf432cbd4147a4a64697df71d35f518a79d
billowqiu <billowqiu@163.com>
2017-03-16 16:51:34 +0800

    add api,www log

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # nginx_log_stat
+实时统计nginx日志，并将指标发送到statsd
+使用方式：
+    python ngx_tail.py config --access-log=/data/wwwlogs/xxx.log --vhost-prefix=wechat --statsd-port=8225
+    vhost-prefix是为了区分一个机器上面有多个nginx日志
--- restart.sh
+++ restart.sh
@@ -3,11 +3,14 @@
 # Author: billowqiu
 # mail: billowqiu@163.com
 # Created Time: 2016-10-06 13:21:57
-# Last Changed: 2016-10-27 17:33:16
+# Last Changed: 2017-03-16 16:50:11
 #########################################################################
 #!/bin/bash
-cd /home/work/tmp/ngx_tail/
+basepath=$(cd `dirname $0`; pwd)
+cd $basepath
 logfile=ngx_tail.log
+
+echo "$basepath" >> ${logfile}
 pids=`ps -ef | grep python | grep ngx_tail | grep -v grep | awk '{print $2}'`
 echo "`date` ${pids}" >> ${logfile}
 status=`echo ${pids} | wc -l`
@@ -22,7 +25,8 @@ fi
 
 #then start it
 source /home/work/.bash_profile
-nohup python ngx_tail.py -l /data/wwwlogs/wechat.51ekt.com_nginx.log &
-nohup python ngx_tail_pc.py -l /data/wwwlogs/www.51ekt.com_nginx.log &
+nohup python ${basepath}/ngx_tail.py config --access-log=/data/wwwlogs/wechat.51ekt.com_nginx.log --vhost-prefix=wechat --statsd-port=8125 &
+nohup python ${basepath}/ngx_tail.py config --access-log=/data/wwwlogs/www.51ekt.com_nginx.log --vhost-prefix=www --statsd-port=8125 &
+nohup python ${basepath}/ngx_tail.py config --access-log=/data/wwwlogs/api.51ekt.com_nginx.log --vhost-prefix=api --statsd-port=8125 &
 echo "`date` restarted ngx_tail" >> ${logfile}
 
